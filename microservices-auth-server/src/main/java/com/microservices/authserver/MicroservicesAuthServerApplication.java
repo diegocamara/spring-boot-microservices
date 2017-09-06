6bde2b94c353713cac0e9a1ec030ed84c8bc8fe8
diegocamara <diegoluizjava.java@gmail.com>
2017-09-05 21:10:44 -0300

    Inclusão do auth-server

---
+++ microservices-auth-server/src/main/java/com/microservices/authserver/MicroservicesAuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.microservices.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesAuthServerApplication.class, args);
+	}
+}
