65fbcc6cadc13b5e492979e03c9e69ebd9714f71
Diego Câmara <diegoluizjava.java@gmail.com>
2019-03-06 19:15:25 -0300

    Ajustes na estrutura do projeto

--- microservices-auth-server/src/main/java/com/microservices/authserver/domain/User.java
+++ microservices-auth-server/src/main/java/com/microservices/authserver/domain/User.java
@@ -1,60 +1,63 @@
 package com.microservices.authserver.domain;
 
-import java.util.Collection;
-
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+
 @Document(collection = "users")
 public class User implements UserDetails {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String username;
-	private String password;
+    @Id
+    private String username;
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
-	}
+    private String password;
 
-	@Override
-	public String getPassword() {
-		return this.password;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 
-	@Override
-	public String getUsername() {
-		return this.username;
-	}
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
--- microservices-auth-server/src/main/java/com/microservices/authserver/service/security/MongoUserDetailsService.java
+++ microservices-auth-server/src/main/java/com/microservices/authserver/service/security/MongoUserDetailsService.java
@@ -1,30 +1,31 @@
 package com.microservices.authserver.service.security;
 
+import com.microservices.authserver.domain.User;
+import com.microservices.authserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.microservices.authserver.domain.User;
-import com.microservices.authserver.repository.UserRepository;
+import java.util.Optional;
 
 @Service
 public class MongoUserDetailsService implements UserDetailsService {
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Override
-	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 
-		User user = this.userRepository.findOne(userName);
+        Optional<User> user = this.userRepository.findById(userName);
 
-		if (user == null) {
-			throw new UsernameNotFoundException(userName);
-		}
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException(userName);
+        }
 
-		return user;
-	}
+        return user.get();
+    }
 
 }
